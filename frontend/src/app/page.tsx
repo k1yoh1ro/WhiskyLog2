9c8e04309099f4dd3c689ff2879933f4a7a99eaf
kiyota <kiyota@feel-s.co.jp>
2024-09-01 15:28:42 +0900

    add login pages

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,30 +1,30 @@
-'use client'
+// 'use client'
 
-import { test } from "@/app/api/api";
-import { useEffect } from "react";
+// import { login } from "@/app/api/api";
+// import { useEffect } from "react";
 
-export default function Home() {
-  let testString = "";
+// export default function Home() {
+//   let testString = "";
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const tmpStr = await test();
-        testString = tmpStr;
-      } catch (error) {
+//   useEffect(() => {
+//     async function fetchData() {
+//       try {
+//         const tmpStr = await test();
+//         testString = tmpStr;
+//       } catch (error) {
 
-      }
-    }
+//       }
+//     }
 
-    fetchData();
-  }, [])
+//     fetchData();
+//   }, [])
 
-  return (
-    <>
-      <div className="container">
-        <div>{testString}</div>
-        <div>testdddd</div>
-      </div>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <div className="container">
+//         <div>{testString}</div>
+//         <div>testdddd</div>
+//       </div>
+//     </>
+//   )
+// }
